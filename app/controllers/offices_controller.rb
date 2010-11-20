f1a26f4d2d794d41947a8beaeaab61ae32513aaa
Pablo Villalba <pablo@teambox.com>
2010-11-20 13:02:22 +0100

    Scaffolding offices so we can play with some data

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,9 +1,45 @@
 class OfficesController < ApplicationController
 
   def index
+    @offices = Office.all
   end
 
   def show
+    @office = Office.find(params[:id])
   end
 
+  def new
+    @office = Office.new
+  end
+
+  def edit
+    @office = Office.find(params[:id])
+  end
+
+  def create
+    @office = Office.new(params[:office])
+
+    if @office.save
+      redirect_to(@office, :notice => 'Office was successfully created.')
+    else
+      render :action => "new"
+    end
+  end
+
+  def update
+    @office = Office.find(params[:id])
+
+    if @office.update_attributes(params[:office])
+      redirect_to(@office, :notice => 'Office was successfully updated.')
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @office = Office.find(params[:id])
+    @office.destroy
+
+    redirect_to(offices_url)
+  end
 end
