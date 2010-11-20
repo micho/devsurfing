3398d80ad38a221912b8374561adf2a672ee92d7
Pablo Villalba <pablo@teambox.com>
2010-11-20 11:52:27 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devsurfing::Application.config.secret_token = 'fa2d89b8ad8a2c05cba05b1145badddc7cb27c9dea0db5fa323c8536c5c8ad552bbf30ad561dc371c6a2702ee22f27289c07be4a4b4ef4269597251b1ac96382'
