131f55d46f868bcadebd222b0ddd9ddd7c8c083e
Pablo Villalba <pablo@teambox.com>
2010-11-20 15:46:24 +0100

    Search works, now using fulltext sql search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
 
   def show
+    @offices = Office.find_by_search_terms(params[:location])
   end
 
 end
--- app/models/office.rb
+++ app/models/office.rb
@@ -5,4 +5,8 @@ class Office < ActiveRecord::Base
 
   validates_numericality_of :desks
 
+  def self.find_by_search_terms(terms)
+    Office.find_by_sql("SELECT * FROM offices WHERE name like '%#{terms}%'")
+  end
+
 end
